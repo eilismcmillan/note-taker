e8151b9a6058e5aeb7d85040bb94ccf0727e21ff
eilismcmillan <eilis.mcmillan1998@gmail.com>
2023-05-12 18:48:32 +1000

    updated server and routes

---
+++ Develop/routes/notes.js
@@ -0,0 +1,61 @@
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const {
+    readFromFile,
+    readAndAppend,
+    writeToFile,
+  } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+notes.get('/', (req, res) => {
+});
+
+notes.get('/:note_id', (req, res) => {
+    const noteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.note_id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const {title, text} = req.body;
+  
+    if (req.body) {
+      const newNote = {
+        title,
+        text,
+        note_id: uuidv4(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+      res.json(`New note added successfully 📝`);
+    } else {
+      res.error('Error in adding new note');
+    }
+  });
+
+
+notes.delete('/:note_id', (req, res) => {
+    const deletedNoteId = req.params.note_id;
+    readFromFile('./db/db.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.note_id !== noteId);
+  
+        writeToFile('./db/db.json', result);
+  
+        res.json(`Item ${deletedoteId} has been deleted 🗑️`);
+      });
+  });
+
+  module.exports = notes;
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const api = require('./develop/routes/index.js');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'develop/public/index.html'))
+});
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'develop/public/notes.html'))
+});
+
+app.listen(PORT, () =>
+  console.log(`Note taker app listening at http://localhost:${PORT}`)
+);
